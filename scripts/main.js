0bb2f1050dc35570b2fe6342087e7430b8804c7c
jessicadever1 <jessicadever1@gmail.com>
2020-12-04 15:25:42 -0600

    added event listener and filtered data

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,32 @@
-import { criminalList } from './criminals/criminalList.js'
-criminalList()
+import { CriminalList } from './criminals/criminalList.js'
+CriminalList()
 
 
 import { ConvictionSelect } from './convictions/convictionSelect.js'
 ConvictionSelect()
+
+/*
+What are we trying to do today?
+
+On 12/4/20 - we're trying to filter our criminals
+by the crime they committed. 
+
+
+What tasks do we need to complete, to implement
+the feature?
+    filter through the criminals data by matching
+    the crime that was selected
+
+    1. Listen for the selection of a crime and 
+       capture the chosen value
+    2. Use the selected value to filter the 
+       criminal data
+    3. Update the DOM with the filtered criminal
+       data
+
+Which modules are involved?
+CriminalList updates the DOM with the filtered 
+    criminal data
+
+ConvictionSelect 
+*/
