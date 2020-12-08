e35d06a456f295339a4591775a1b38586415348d
jessicadever1 <jessicadever1@gmail.com>
2020-12-08 13:31:40 -0600

    added CSS, a note.js, a noteList.js and import in main.js

--- scripts/notes/noteDataProvider.js
+++ scripts/notes/noteDataProvider.js
@@ -8,7 +8,11 @@ const dispatchStateChangeEvent = () => {
     eventHub.dispatchEvent(noteStateChangedEvent)
 }
 
-const getNotes = () => {
+export const useNotes = () => {
+    return notes.slice()
+}
+
+export const getNotes = () => {
     return fetch('http://localhost:8088/notes')
         .then(response => response.json())
         .then(parsedNotes => {
